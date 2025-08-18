c14e2b3859b189c8d6ad2261f83b761aa0e25097
Guest <guest1@ray.net>
2025-08-18 05:57:06 -0700

    created productPage and product cards with detailes

--- src/app/pages/products/products.ts
+++ src/app/pages/products/products.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Product, Productservice } from '../../services/productservice';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-products',
   standalone:true,
-  imports: [CommonModule],
+  imports: [CommonModule,FormsModule],
   templateUrl: './products.html',
   styleUrl: './products.css'
 })
@@ -13,6 +14,21 @@ export class Products implements OnInit {
 
   products:Product[]=[];
   loading:boolean=true;
+
+  // create newProduct in modal
+  showModal=false;
+  newProduct={
+    id:'',
+    title:'',
+    description:'',
+    image:'',
+  }
+
+  // edit modal
+  showEditModal=false;
+  editProductData={id:'',title:'',description:'',image:''}
+
+
   constructor(private productservice:Productservice){}
 
   ngOnInit(): void {
@@ -30,6 +46,65 @@ export class Products implements OnInit {
         console.error('خطا در دیافت داده ها',err);
         this.loading=false;
       }
+    })
+  }
+
+  // addProduct():void{
+  //   const newProduct={
+  //     id:String(Date.now()),
+  //     title:' New Product',
+  //     description:'Sample description',
+  //     image:'https://picsum.photos/300/200?random='+Date.now()
+  //   }
+  //   this.productservice.create(newProduct).subscribe(()=>{
+  //     this.products.push(newProduct);
+  //   })
+  // }
+
+
+
+  openModal():void{
+    this.showModal=true;
+    this.newProduct={id:'',title:'',description:'',image:''}
+  }
+  closeModal():void{
+    this.showModal=false
+  }
+
+  saveProduct():void{
+    this.newProduct.id=String(Date.now());
+    this.newProduct.image='https://picsum.photos/300/200?random='+Date.now();
+    this.productservice.create(this.newProduct).subscribe(()=>{
+      this.products.push({...this.newProduct});
+      this.closeModal()
+    })
+  }
+
+
+  // open edit modal
+  openEditModal(product:Product):void{
+    this.editProductData={...product};
+    this.showEditModal=true;
+  }
+
+  // close edit modal
+  closeEditModal(){
+    this.showEditModal=false;
+  }
+
+  editProduct(){
+    this.productservice.update(this.editProductData).subscribe(()=>{
+    this.products=this.products.map( p=>
+    p.id===this.editProductData.id ? this.editProductData : p);
+
+    this.closeEditModal();
+    })
+    
+  }
+
+  deleteProduct(id:string):void{
+    this.productservice.delete(id).subscribe(()=>{
+      this.products=this.products.filter(p=>p.id !==id)
     })
   }
 }
