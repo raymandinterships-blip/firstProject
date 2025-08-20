237fa847e352cb7a7a249c887eac17851a116717
Guest <guest1@ray.net>
2025-08-20 00:20:00 -0700

    guard service created

--- src/app/pages/userform/userform.ts
+++ src/app/pages/userform/userform.ts
@@ -5,34 +5,34 @@ import { AuthService } from '../../services/auth-service';
 
 @Component({
   selector: 'app-userform',
-  imports: [ReactiveFormsModule,NgIf],
-  standalone:true,
+  imports: [ReactiveFormsModule, NgIf],
+  standalone: true,
   templateUrl: './userform.html',
-  styleUrl: './userform.css'
+  styleUrl: './userform.css',
 })
 export class UserForm {
-  myForm:FormGroup;
-  constructor(private fb:FormBuilder  ,private authService:AuthService){
-    this.myForm=this.fb.group({
-      username:['',Validators.required],
-      password:['',Validators.required]
+  myForm: FormGroup;
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+    this.myForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(6)]],
       // lastName:['',Validators.required],
       // email:['',[Validators.required,Validators.email]],
       // phoneNumber:['',[Validators.required,Validators.pattern(/^(\+98|0)?9\d{9}$/)]]
-    })
+    });
   }
-  onSubmit(){
-    if(this.myForm.valid){
-      this.authService.create(this.myForm.value).subscribe({
-        next:(res)=>{
-          console.log("کاربر با موفقیت ساخته شد",res)
-        },error:(err)=>{
-          console.log("خطادرایجادکاربر",err)
-        }
-      })
-    }else{
-      console.log("فرم نا معتبر است")
+  onSubmit() {
+    if (this.myForm.valid) {
+      this.authService.login(this.myForm.value).subscribe({
+        next: (res) => {
+          console.log('کاربر با موفقیت وارد شد', res);
+        },
+        error: (err) => {
+          console.log('خطا در ورود کاربر', err);
+        },
+      });
+    } else {
+      console.log('فرم نا معتبر است');
     }
   }
-
 }
--- src/app/services/auth-service.ts
+++ src/app/services/auth-service.ts
@@ -1,22 +1,27 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
 export interface User {
-  id: string;
-  firstName: string;
-  lastName: string;
-  email: string;
-  phoneNumber: string;
+  username: string;
+  password: string;
+  token:string
+  // id: string;
+  // lastName: string;
+  // email: string;
+  // phoneNumber: string;
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-private  authUrl = 'http://192.168.180.181/users/login';
-  constructor(private http:HttpClient){}
-  create(user:User):Observable<User>{
-    return this.http.post<User>(this.authUrl,user)
+  private authUrl = 'http://192.168.180.181:1234/users/login';
+  constructor(private http: HttpClient) {}
+  login(user: User): Observable<User> {
+    const headers =new HttpHeaders({
+      'Content-Type':'application/json'
+    })
+    return this.http.post<User>(this.authUrl, user ,{headers});
   }
 }
